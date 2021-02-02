9061a191ad9ed206263429ef70b2f284460542d3
Ratik <aleksandr-ratkevich@mail.ru>
2021-02-02 15:01:42 +0300

    With BLOG model

--- My_awesome_BLOG/settings.py
+++ My_awesome_BLOG/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
     'events.apps.EventsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -76,14 +77,14 @@ WSGI_APPLICATION = 'My_awesome_BLOG.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE':'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3'
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'myawesomeblogdb',
-        'USER': 'postgres',
-        'PASSWORD':'1234',
-        'HOST':'127.0.0.1',
-        'PORT':'5432',
+        'ENGINE':'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3'
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': 'myawesomeblogdb',
+        # 'USER': 'postgres',
+        # 'PASSWORD':'1234',
+        # 'HOST':'127.0.0.1',
+        # 'PORT':'5432',
     }
 }
 
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+	blog_image = models.ImageField(upload_to='blog_images/')
+	blog_text = models.CharField(max_length=5000)
