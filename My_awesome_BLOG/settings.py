ffc115a32def5157986565c4f04abb55d209a799
Ratik <aleksandr-ratkevich@mail.ru>
2021-02-02 16:51:53 +0300

    With date

--- My_awesome_BLOG/settings.py
+++ My_awesome_BLOG/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'blog.apps.PostsConfig',
+    'blog.apps.BlogConfig',
     'events.apps.EventsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,11 @@
 from django.db import models
+from datetime import datetime, date
 
 # Create your models here.
-class Posts(models.Model):
-	posts_image = models.ImageField(upload_to='posts_images/')
-	posts_text = models.CharField(max_length=5000)
+
+class Post(models.Model):
+	post_h = models.CharField(max_length=44)
+	date_of_posts = models.DateField(auto_now_add=False, auto_now=False, blank=True)
+	text_of_posr = models.CharField(max_length=2000)
+	post_image = models.ImageField(upload_to='post_image/')
+
